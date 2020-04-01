3d0df37257e42930a5e82ebc0792fd36192deaae
Roman Nikitin <nikrom2012@me.com>
2020-04-01 21:12:59 +0300

    Moved logic to the ConfigGenerator class

--- gen.py
+++ gen.py
@@ -5,19 +5,12 @@ import os
 import re
 import shutil
 
-from jinja2 import Environment, FileSystemLoader, select_autoescape
-
 if __name__ == '__main__':
     import config
-    import crypto_utils
+    import generator
 else:
+    from . import generator
     from . import config
-    from . import crypto_utils
-
-jenv = Environment(
-    loader=FileSystemLoader(config.TEMPLATES_PATH),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
 
 def initialize():
@@ -33,135 +26,19 @@ def initialize():
     os.makedirs(config.JURY_CLIENT_DIR, exist_ok=True)
 
 
-def generate(team_list, per_team, vpn_server, gen_team, gen_jury, gen_vuln):
-    dhparam = crypto_utils.get_dhparam()
-
-    ca_cert, ca_key = crypto_utils.create_ca(CN='cbsctf.live')
-    ca_cert_dump = crypto_utils.dump_file_in_mem(ca_cert).decode()
-
-    if gen_team or gen_vuln:
-        for team_num in team_list:
-            formatted_team = str(team_num).zfill(3)
-            if gen_team:
-                team_static_key = crypto_utils.generate_static_key()
-
-                team_client_dir = os.path.join(config.TEAM_CLIENT_DIR, f'team{formatted_team}')
-                os.makedirs(team_client_dir, exist_ok=True)
-
-                for person in range(1, per_team + 1):
-                    client_name = f'team{formatted_team}_{person}'
-
-                    cert, key = crypto_utils.generate_subnet_certs(
-                        ca_cert=ca_cert,
-                        ca_key=ca_key,
-                        client_name=client_name,
-                        serial=0x0C,
-                        is_server=False,
-                    )
-
-                    template = jenv.get_template('team_client.j2')
-                    rendered = template.render(
-                        config=config,
-                        server_host=vpn_server,
-                        team_num=team_num,
-                        ca_cert=ca_cert_dump,
-                        cert=cert,
-                        key=key,
-                        static_key=team_static_key,
-                    )
-
-                    ovpn_dump_path = os.path.join(team_client_dir, f'{client_name}.ovpn')
-                    with open(ovpn_dump_path, 'w') as f:
-                        f.write(rendered)
-
-                server_name = f'team_server{formatted_team}'
-                cert, key = crypto_utils.generate_subnet_certs(
-                    ca_cert=ca_cert,
-                    ca_key=ca_key,
-                    client_name=server_name,
-                    serial=0x0C,
-                    is_server=True,
-                )
-
-                template = jenv.get_template('team_server.j2')
-                rendered = template.render(
-                    config=config,
-                    server_host=vpn_server,
-                    team_num=team_num,
-                    ca_cert=ca_cert_dump,
-                    cert=cert,
-                    key=key,
-                    static_key=team_static_key,
-                    dhparam=dhparam,
-                )
-
-                conf_dump_path = os.path.join(config.TEAM_SERVER_DIR, f'{server_name}.conf')
-                with open(conf_dump_path, 'w') as f:
-                    f.write(rendered)
-
-            if gen_vuln:
-                vulnbox_static_key = crypto_utils.generate_static_key()
-
-                template = jenv.get_template('vuln_client.j2')
-                rendered = template.render(
-                    config=config,
-                    server_host=vpn_server,
-                    team_num=team_num,
-                    static_key=vulnbox_static_key,
-                )
-
-                ovpn_dump_path = os.path.join(config.VULN_CLIENT_DIR, f'vuln{team_num}.ovpn')
-                with open(ovpn_dump_path, 'w') as f:
-                    f.write(rendered)
-
-                template = jenv.get_template('vuln_server.j2')
-                rendered = template.render(
-                    config=config,
-                    server_host=vpn_server,
-                    team_num=team_num,
-                    static_key=vulnbox_static_key,
-                )
-
-                ovpn_dump_path = os.path.join(config.VULN_SERVER_DIR, f'vuln_server{formatted_team}.conf')
-                with open(ovpn_dump_path, 'w') as f:
-                    f.write(rendered)
-
-    if gen_jury:
-        jury_static_key = crypto_utils.generate_static_key()
-
-        template = jenv.get_template('jury_client.j2')
-        rendered = template.render(
-            config=config,
-            server_host=vpn_server,
-            static_key=jury_static_key,
-        )
-
-        ovpn_dump_path = os.path.join(config.JURY_CLIENT_DIR, f'config.ovpn')
-        with open(ovpn_dump_path, 'w') as f:
-            f.write(rendered)
-
-        template = jenv.get_template('jury_server.j2')
-        rendered = template.render(
-            config=config,
-            server_host=vpn_server,
-            static_key=jury_static_key,
-        )
-
-        ovpn_dump_path = os.path.join(config.JURY_SERVER_DIR, f'jury.conf')
-        with open(ovpn_dump_path, 'w') as f:
-            f.write(rendered)
-
-
 def run(team_list, per_team, vpn_server, gen_team=True, gen_jury=True, gen_vuln=True):
     initialize()
-    generate(
-        team_list=team_list,
-        per_team=per_team,
-        vpn_server=vpn_server,
-        gen_team=gen_team,
-        gen_jury=gen_jury,
-        gen_vuln=gen_vuln,
-    )
+
+    cg = generator.ConfigGenerator(vpn_server=vpn_server, cn='cbsctf.live')
+
+    if gen_team:
+        cg.generate_for_teams(team_list=team_list, per_team=per_team)
+
+    if gen_vuln:
+        cg.generate_for_vulns(team_list)
+
+    if gen_jury:
+        cg.generate_for_jury()
 
 
 if __name__ == "__main__":
